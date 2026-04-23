b445772daad4515248068d462f0111d8a6e27422
Zabdi <pel25586@uvg.edu.gt>
2026-04-23 11:37:15 -0600

    Se agregó el método search y la llegada aleatoria de procesos

--- BST.py
+++ BST.py
@@ -1,3 +1,43 @@
+from nodo import Nodo
+
 class BST:
     def __init__(self):
         self.root = None
+
+    def insert(self, proceso):
+        nuevo = Nodo(proceso)
+
+        if self.root is None:
+            self.root = nuevo
+            return
+
+        current = self.root
+        while True:
+            if proceso.vruntime < current.proceso.vruntime:
+                if current.left is None:
+                    current.left = nuevo
+                    nuevo.parent = current
+                    return
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = nuevo
+                    nuevo.parent = current
+                    return
+                current = current.right
+
+    def search(self, vruntime):
+        current = self.root
+        steps = 0
+
+        while current:
+            steps += 1
+
+            if vruntime == current.proceso.vruntime:
+                return current, steps
+            elif vruntime < current.proceso.vruntime:
+                current = current.left
+            else:
+                current = current.right
+
+        return None, steps
